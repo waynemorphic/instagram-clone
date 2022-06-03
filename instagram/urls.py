977d248cecea1706fb8a84eeca4225d5fb69fcc4
waynemorphic <wayneklyde@gmail.com>
2022-06-03 21:51:31 +0300

    web application urls for registration and loggin in

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    # account registration
+    path('accounts/register/', RegistrationView.as_view(success_url = '/home/')),
+    path('accounts/login/', views.login, name='accounts/login/'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

e6b82da06e89da1badf75bc50d7dd4dd03e4060f
waynemorphic <wayneklyde@gmail.com>
2022-06-03 13:12:25 +0300

    index function and a render response fucntion

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# index function to render index template file
+def index(request):
+    return render(request, 'home/index.html')

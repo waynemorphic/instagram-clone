17e4ad848e74e44a01a6b5e0ff8c18dbcb455272
waynemorphic <wayneklyde@gmail.com>
2022-06-03 21:53:53 +0300

    migrations, url alterations and template inheritances

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from core import views 
+from django.conf.urls.static import static
+from django.conf import settings
 
 # url pattern
 urlpatterns = [
     path('', views.index, name = 'index'),
+   
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

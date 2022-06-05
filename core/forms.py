8165e6de282fdcc0ceb42334a30cc35f1a0c60f6
waynemorphic <wayneklyde@gmail.com>
2022-06-06 00:43:56 +0300

    profile, post and user forms

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
-from django import forms
 
+from django import forms
+from core.models import User_Model, Profile_Model, Image_Model
 class UserRegistrationForm(forms.Form):
     email = forms.EmailField(label = 'Email', required = True)
     full_name = forms.CharField(label= 'Full Name', min_length = 4, required= True)
@@ -9,3 +10,15 @@ class UserRegistrationForm(forms.Form):
 class UserLoginForm(forms.Form):
     email = forms.EmailField(label = 'Email', required = True)
     password = forms.CharField(label = 'Password', required = True)
+    
+class NewPostForm(forms.Form):
+    image = forms.ImageField(label = 'Upload Image')
+    image_name = forms.CharField(label = 'Image Name', max_length=50)
+    image_caption = forms.CharField(label = 'Image Caption', max_length = 100)
+
+class CommentForm(forms.Form):
+    comments = forms.CharField(widget = forms.Textarea)
+
+class ProfileForm(forms.Form):
+    profile_picture = forms.ImageField(label = 'Upload Profile Picture')
+    bio = forms.Textarea()

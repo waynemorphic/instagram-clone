4782dd1cb51e4f2404b68fb1145213e58c3afe06
waynemorphic <wayneklyde@gmail.com>
2022-06-06 15:36:08 +0300

    forms DRY in profile and image classes

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,8 @@
 
 from django import forms
 from core.models import User_Model, Profile_Model, Image_Model
+
+
 class UserRegistrationForm(forms.Form):
     email = forms.EmailField(label = 'Email', required = True)
     full_name = forms.CharField(label= 'Full Name', min_length = 4, required= True)
@@ -12,13 +14,19 @@ class UserLoginForm(forms.Form):
     password = forms.CharField(label = 'Password', required = True)
     
 class NewPostForm(forms.Form):
-    image = forms.ImageField(label = 'Upload Image')
-    image_name = forms.CharField(label = 'Image Name', max_length=50)
-    image_caption = forms.CharField(label = 'Image Caption', max_length = 100)
+    class Meta:
+        model = Image_Model
+        exclude = ['user', 'post_date', 'likes', 'comments', 'profile']
+    # image = forms.ImageField(label = 'Upload Image')
+    # image_name = forms.CharField(label = 'Image Name', max_length=50)
+    # image_caption = forms.CharField(label = 'Image Caption', max_length = 100)
 
 class CommentForm(forms.Form):
     comments = forms.CharField(widget = forms.Textarea)
 
 class ProfileForm(forms.Form):
-    profile_picture = forms.ImageField(label = 'Upload Profile Picture')
-    bio = forms.Textarea()
+    class Meta:
+        model = Profile_Model
+        exclude = ['user']
+    # profile_picture = forms.ImageField(label = 'Upload Profile Picture')
+    # bio = forms.Textarea()

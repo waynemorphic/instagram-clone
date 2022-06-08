4ed74fbfabf7ab55322638c59e2fbbf10d29d52f
waynemorphic <wayneklyde@gmail.com>
2022-06-08 14:22:55 +0300

    admin registration of models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from core.models import Image_Model, Profile_Model, User_Model
 # Register your models here.
+
+
+
+# class ImageAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['image_name']}),
+#         ('user', {'fields': ['user']}),
+#         ('image caption', {'fields': ['image_caption']}),
+#     ]
+    
+admin.site.register(Image_Model)
+admin.site.register(Profile_Model)
+admin.site.register(User_Model)

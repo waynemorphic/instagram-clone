649910017880b6e17d5ac2548dd0dfe2cc88317a
waynemorphic <wayneklyde@gmail.com>
2022-06-08 14:23:38 +0300

    changes to the model fields

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from tinymce.models import HTMLField 
-
+from tinymce.models import models 
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
 
 # user model
@@ -26,9 +27,9 @@ class Profile_Model(models.Model):
         profile_photo, bio, user
     '''
 
-    profile_photo = models.ImageField(upload_to = 'static/media', default = '')
-    bio = HTMLField()
-    user = models.ForeignKey(User_Model, on_delete = models.CASCADE, null=True)
+    profile_photo = CloudinaryField('post image', null=True)
+    bio = models.CharField(max_length= 250)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
 
     def __str__(self):
         return self.bio
@@ -48,14 +49,14 @@ class Image_Model(models.Model):
         image, image_name, image_caption, likes, comments, post_date, profile, user
     '''
     
-    image = models.ImageField(upload_to = 'pictures/', default = '')
+    image = CloudinaryField('post image', null=True)
     image_name = models.CharField(max_length = 50)
-    image_caption = HTMLField()
+    image_caption = models.CharField(max_length= 250)
     likes = models.IntegerField(null = True)
-    comments = HTMLField(null = True)
+    comments = models.CharField(max_length= 250, null = True)
     post_date = models.DateTimeField(auto_now = True)
     profile = models.ForeignKey(Profile_Model, on_delete = models.CASCADE, null = True)
-    user = models.ForeignKey(User_Model, on_delete = models.CASCADE, null = True)    
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True)    
     
     def __str__(self):
         return self.image_name
